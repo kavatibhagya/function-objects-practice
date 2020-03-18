f98565eb53f4fe8cc02329ea367f36d9bd0641a1
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-03-17 20:45:45 -0700

    added avg of array

---
+++ avgarray.js
@@ -0,0 +1,17 @@
+let grades = [34, 56, 78, 4, 23, 12, 14, 44];
+       
+    let total = 0;
+   
+     let avg;
+
+
+    
+ for(i=0; i<grades.length; i++){
+     
+     total += grades[i];
+ }
+    avg = total/(grades.length);
+
+
+ 
+ console.log(avg);
--- searchString.js
+++ searchString.js
@@ -1,7 +1,8 @@
-let myString = "search the string baby";
+let myString = "search the string baby c c c c ";
 let searchString = "c";
 for(i=0; i<myString.length; i++){
     if(myString[i]===searchString){
         console.log(myString[i] + " is found at " +i);
+        break;
     }
 }
