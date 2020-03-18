f934a85c3562c8f643cb7cd15ecc1d41e56db67a
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-03-17 20:49:43 -0700

    added avg of array in another method

--- avgarray.js
+++ avgarray.js
@@ -3,14 +3,21 @@ let grades = [34, 56, 78, 4, 23, 12, 14, 44];
     let total = 0;
    
      let avg;
+     let count = 0;
+     for (i=0; i<grades.length; i++){
+         total += grades[i];
+         count++;
+
+     }
+     avg = total/count;
 
 
     
- for(i=0; i<grades.length; i++){
+// for(i=0; i<grades.length; i++){
      
-     total += grades[i];
- }
-    avg = total/(grades.length);
+ //    total += grades[i];
+ //}
+  //  avg = total/(grades.length);
 
 
  
